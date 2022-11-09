99e714526f8ca70f2fba3919b37d3b5641ff92f6
Rebergues Alexis <alexis.rebergues@etu.imt-nord-europe.fr>
2022-11-09 12:14:21 +0100

    dernier commit

--- TP-04-liste-chainee/V2/linkedListOfMusic.c
+++ TP-04-liste-chainee/V2/linkedListOfMusic.c
@@ -1,16 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "linkedListOfMusic.h"
 
-
-
-#define TODO NULL;
-
-
-
-
-
-extern void afficheElement(Element e) {
+ void afficheElement(Element e) {
         Music* p=(Music*) e;
         printf("%s ", p->name);
         printf("%s ", p->artiste);
@@ -22,12 +15,11 @@ extern void afficheElement(Element e) {
 }
 
 
-extern void detruireElement(Element e) {}
-
+ void detruireElement(Element e) {}
 
 
 // compare deux elements
-extern bool equalsElement(Element e1 , Element e2){
+ bool equalsElement(Element e1 , Element e2){
         Music* p=(Music*)e2;
         Music* p2=(Music*)e1;
         return (strcmp(p2->album,p->album)==0)&&(strcmp(p2->artiste,p->artiste)==0)&&(strcmp(p2->genre,p->genre)==0)&&(strcmp(p2->name,p->name)==0)&&(p2->number==p->number)&&(p2->tracknumber==p->tracknumber)&&(p2->year==p->year);
--- TP-04-liste-chainee/V2/linkedListOfMusic.h
+++ TP-04-liste-chainee/V2/linkedListOfMusic.h
@@ -4,3 +4,17 @@
 #include "linkedList.h"
 
 
+typedef struct
+{
+    char* name ;
+    char* artiste;
+    char* album ;
+    char* genre ;
+    int number;
+    int tracknumber;
+    int year;
+}Music;
+
+void afficheElement(Element e);
+bool equalsElement(Element e1, Element e2);
+void detruireElement(Element e);
