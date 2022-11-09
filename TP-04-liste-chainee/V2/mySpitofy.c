95a3a400482d7438a1e2fcf8a9e8ae5e2c361d81
Rebergues Alexis <alexis.rebergues@etu.imt-nord-europe.fr>
2022-11-09 20:27:02 +0100

    nouveau commit

--- TP-04-liste-chainee/V2/mySpitofy.c
+++ TP-04-liste-chainee/V2/mySpitofy.c
@@ -14,7 +14,19 @@ int main(void){
   music.number=2;
   music.tracknumber=3;
   music.year=2022;
+
+  Music music2;
+  music2.album="jiodfjdsoi";
+  music2.artiste="ifdsjfiodf";
+  music2.genre="jdfsif";
+  music2.name="jfdsjf";
+  music2.number=2;
+  music2.tracknumber=3;
+  music2.year=2022;
+
   afficheElement(&music);
+  bool egal=equalsElement(&music2,&music);
+  printf("%d",egal);
     // char filename[]="music.csv";
     // FILE* f;
     // f=fopen(filename,"r");
