d678d23241f73d1a6706e48b9cf1ac27af27af16
AlexisRebergues <115557979+AlexisRebergues@users.noreply.github.com>
2022-11-12 00:56:11 +0100

    Update linkedListOfMusic.c

--- TP-04-liste-chainee/V2/linkedListOfMusic.c
+++ TP-04-liste-chainee/V2/linkedListOfMusic.c
@@ -32,30 +32,6 @@
         return (strcmp(p2->album,p->album)==0)&&(strcmp(p2->artiste,p->artiste)==0)&&(strcmp(p2->genre,p->genre)==0)&&(strcmp(p2->name,p->name)==0)&&(p2->number==p->number)&&(p2->tracknumber==p->tracknumber)&&(p2->year==p->year);
 }
 
-// void trierListeParAnnee(Liste la)
-// {// ici on va ranger les elements dans l'ordre croisssant avant de les afficher.
-//    Liste temp, temp1, temp3;
-//    Music* min;
-
-//    for(temp=la ; temp!=NULL ; temp=temp->suiv)
-//    {
-//      temp3=temp;
-//      min=(Music*) temp->val;
-
-//         afficheElement(min);
-//      for(temp1=temp->suiv ; temp1!=NULL ; temp1=temp1->suiv)
-//      {
-//         Music* music1=(Music*) temp1->val;
-//         if(min->year > music1->year)
-//         {
-//            temp3=temp1; // le 3è temporaire est l'adresse de l'élement où se trouve le minimum
-//            min=(Music*)temp3->val;
-//         }
-//      }
-//      temp3->val=temp->val; //echange des 2 elements...
-//      temp->val=min;
-//    }
-// }
 
 Liste trierListeParAnnee(Liste L)
 {
@@ -67,13 +43,12 @@ Liste trierListeParAnnee(Liste L)
       while (i->suiv!=NULL)
       {
          min=i; // la valeur de min est initialisée à la valeur de i
-         Music* music2=malloc(sizeof(Music));
-         music2=(Music*) min->val; // afin de pouvoir comparer les annees
+          Music* music2=(Music*) min->val; // afin de pouvoir comparer les annees
          j=i->suiv;
          while (j!=NULL) // on va parcourir l'ensemble des valeurs de la liste pour trouver la valeur minimale pour la position fixée
          {
-             Music* music1=malloc(sizeof(Music));
-              music1= (Music*) j->val;
+            
+              Music* music1= (Music*) j->val;
    
             if (music1->year < music2->year) // on compare comme prévu 
             {
@@ -93,10 +68,7 @@ Liste trierListeParAnnee(Liste L)
       }
       return(L);
       } 
-                
-
-
-               
+             
  
 
 
